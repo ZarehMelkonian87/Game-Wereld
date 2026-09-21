fbdf5e3c5ab7ddbe04ac2409f193a5e19c89e6bb
ZarehMelkonian87 <ZarehMelkonian87@users.noreply.github.com>
2026-09-21 15:35:02 +0000

    deploy: d071bfcc246dbc3a2c5c2103a68791caa400402a

--- assets/ScreenGuard-DhFxTONq.js
+++
@@ -1,1 +0,0 @@
-import{ax as o}from"./game-magisch-strand-avontuur-DM75NINq.js";const r=(s,e)=>{o.useEffect(()=>{s||e("/")},[s,e])};export{r as u};
---
+++ assets/ScreenGuard-fpcmYwgW.js
@@ -0,0 +1,1 @@
+import{ax as o}from"./game-magisch-strand-avontuur-DQMTithM.js";const r=(s,e)=>{o.useEffect(()=>{s||e("/")},[s,e])};export{r as u};
